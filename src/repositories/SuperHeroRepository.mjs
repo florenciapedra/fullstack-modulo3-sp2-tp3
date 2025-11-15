f8fe8120d9bc9cb56f4aa6ec10aaf9678d8c8d35
Florencia Pedraza Aguero <florenciapedrazaaguero@gmail.com>
2025-11-14 22:52:44 -0300

    Agregar ejercicios trabajo práctico 3

---
+++ src/repositories/SuperHeroRepository.mjs
@@ -0,0 +1,30 @@
+import SuperHero from '../models/SuperHero.mjs';
+import IRepository from './IRepository.mjs';
+
+class SuperHeroRepository extends IRepository {
+    async obtenerPorId(id) {
+        return await SuperHero.findById(id);
+    }
+
+    async obtenerTodos() {
+        return await SuperHero.find({});
+    }
+
+    async buscarPorAtributo(atributo, valor) {
+        const query = {};
+        
+        if (!isNaN(valor) && valor !== '') {
+            query[atributo] = Number(valor);
+        } else {
+            query[atributo] = valor;
+        }
+
+        return await SuperHero.find(query);
+    }
+    
+    async obtenerMayoresDe30() {
+        return await SuperHero.find({ edad: { $gt: 30 } });
+    }
+}
+
+export default new SuperHeroRepository();
